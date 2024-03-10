f3f3d3ebcf451dc432ff77d287b1b95ebf45b65a
HaiPan10 <phanthanhhai3317@gmail.com>
2024-03-10 12:19:26 +0700

    [Hai] - api gateway

---
+++ demo-micro/demo-api-gateway/src/main/java/com/example/demoapigateway/DemoApiGatewayApplication.java
@@ -0,0 +1,11 @@
+package com.example.demoapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApiGatewayApplication.class);
+    }
+}
